c7400d55baec7622a98509daafed2e62c8498c82
Q Le Roux <quentin.leroux@edhec.com>
2026-01-12 22:08:27 +0100

    KnR 1.5.3

--- KnR/ch1.c
+++ KnR/ch1.c
@@ -6,16 +6,26 @@
 
 void ch1_11_11();
 void ch1_11_12();
-void ch1_12_11();
-void ch1_15();
+void ch1_12();
+void ch1_15_1();
+void ch1_15_2();
+void ch1_15_3();
+void ch1_15_31();
+void ch1_15_32();
+void ch1_15_33();
 
 // main
 
 int main() {
-    ch1_11_11();
-    ch1_11_12();
-    ch1_12();
-    ch1_15();
+    // ch1_11_11();
+    // ch1_11_12();
+    // ch1_12();
+    // ch1_15_1();
+    // ch1_15_2();
+    // ch1_15_3();
+    // ch1_15_31();
+    // ch1_15_32();
+    ch1_15_33();
 }
  
 // function
@@ -62,10 +72,10 @@ void ch1_12() {
         printf("%4.0fC\t%5.1fF\n", fcelsius, ffahrenheit);
     }
 
-    printf("%d %d %d %d %d\n", sizeof(char)*8, sizeof(short)*8, sizeof(int)*8, sizeof(double)*8, sizeof(float)*8);
+    printf("%ld %ld %ld %ld %ld\n", sizeof(char)*8, sizeof(short)*8, sizeof(int)*8, sizeof(double)*8, sizeof(float)*8);
 }
 
-void ch1_15() {
+void ch1_15_1() {
     int c; // char could be used but int is instead bc: EOF needs a type size big enough. EOF is defined in stdio.h
     // c = getchar();
     // while (c != EOF) { // EOF can be done using Control - d
@@ -78,4 +88,75 @@ void ch1_15() {
     }
     printf("Exiting while loop\n");
     printf("EOF takes the value: %d\n", EOF);
+}
+
+void ch1_15_2() {
+    // count characters
+    long nc;
+    nc = 0;
+    while (getchar() != EOF) {
+        nc++;
+    }
+    printf("\n%ld\n", nc);
+}
+
+void ch1_15_3() {
+    int c, nl;
+    nl = 0;
+    while ((c = getchar()) != EOF) 
+    {
+        if (c == '\n')
+        {
+            ++nl;
+        }
+    }
+    printf("%d\n", nl);
+}
+
+void ch1_15_31() {
+    int c, nl;
+    nl = 0;
+    while ((c = getchar()) != EOF) 
+    {
+        if (c == '\n' || c == ' ' || c == '\t')
+        {
+            ++nl;
+        }
+    }
+    printf("%d\n", nl);
+}
+
+void ch1_15_32() {
+    int c;
+    int check = 0;
+    while ((c = getchar()) != EOF) {
+        if (c != ' ')
+        {
+            putchar(c);
+            check = 0;
+        } 
+        else if (c == ' ' && !check) 
+        {
+            check = 1;
+            putchar(c);
+        }
+    }
+    printf("Exiting while loop\n");
+    printf("EOF takes the value: %d\n", EOF);
+}
+
+void ch1_15_33() {
+    int c;
+    
+    while ((c = getchar()) != EOF) {
+        if (c == '\t'){
+            printf("\\t");
+        } else if (c == '\b') {
+            printf("\\b");
+        } else if (c == '\\') {
+            printf("\\\\");
+        } else {
+            putchar(c);
+        }
+    }
 }
